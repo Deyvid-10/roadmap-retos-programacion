4da7d8cfbbba6dbf36829aa76210ad0ba36921b2
Ing. Deyvid Marmolejo <140911894+Deyvid-10@users.noreply.github.com>
2024-05-14 23:01:40 -0400

    Create Deyvid-10.js
    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Deyvid-10.js
@@ -0,0 +1,26 @@
+// URL del sitio web oficial de JavaScript: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Este es un comentario en linea
+
+/* Y esta es la manera 
+en la que se hacen comentarios 
+en varias lineas */
+
+// Creacion de variable y constante
+
+let variable = "variable"
+var variable2 = "variable 2"
+const constante = "constante"
+
+// Variables representando todos los tipos de datos primitivos
+
+let cadenaTexto = "Cadena de texto"
+let entero = 0
+let booleano = true
+let nulo = null
+let indefinido = undefined
+
+
+// Imprimir por terminal
+
+console.log("¡Hola, JavaScript!");
